b6580cba0997ac26e077e8c3bab87a97e48701d6
Kondo Takeo <takeoverjp@gmail.com>
2021-10-16 06:07:40 +0900

    refactor: parse arguments in lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+use getopts::Options;
+use std::env;
+use std::process;
+
+pub fn run(config: Config) -> Result<(), String> {
+  println!("delay_sec = {}", config.delay_sec);
+  println!("devices = {:?}", config.devices);
+  Ok(())
+}
+
+pub struct Config {
+  delay_sec: u32,
+  devices: Vec<String>,
+}
+
+fn print_usage(program: &str, opts: &Options) {
+  let brief = format!(
+    "Usage: {} [options] [DEVICE [DEVICE [DEVICE ...]]]",
+    program
+  );
+  print!("{}", opts.usage(&brief));
+}
+
+impl Config {
+  pub fn new(mut args: env::Args) -> Result<Config, String> {
+    let args: Vec<String> = args.collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt(
+      "d",
+      "delay",
+      "delay for update refresh rate in seconds. default is 1",
+      "SECONDS",
+    );
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+      Ok(m) => m,
+      Err(err) => {
+        print_usage(&program, &opts);
+        return Err(format!("{}", err));
+      }
+    };
+
+    if matches.opt_present("h") {
+      print_usage(&program, &opts);
+      process::exit(0);
+    }
+
+    let delay_sec = match matches.opt_get_default::<u32>("d", 0) {
+      Ok(m) => m,
+      Err(err) => {
+        print_usage(&program, &opts);
+        return Err(format!("{}", err));
+      }
+    };
+
+    let devices = matches.free.clone();
+    Ok(Config { delay_sec, devices })
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,18 @@
 extern crate getopts;
-use getopts::Options;
+
 use std::env;
 use std::process;
 
-fn print_usage(program: &str, opts: &Options) {
-    let brief = format!(
-        "Usage: {} [options] [DEVICE [DEVICE [DEVICE ...]]]",
-        program
-    );
-    print!("{}", opts.usage(&brief));
-}
+use iptop::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
-
-    let mut opts = Options::new();
-    opts.optopt(
-        "d",
-        "delay",
-        "delay for update refresh rate in seconds. default is 1",
-        "SECONDS",
-    );
-    opts.optflag("h", "help", "print this help menu");
-    let matches = opts.parse(&args[1..]).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
-        print_usage(&program, &opts);
         process::exit(1);
     });
-    if matches.opt_present("h") {
-        print_usage(&program, &opts);
-        process::exit(0);
+
+    if let Err(err) = iptop::run(config) {
+        eprintln!("Application error: {}", err);
+        process::exit(1);
     }
-
-    let delay_sec = matches
-        .opt_get_default::<u32>("d", 0)
-        .unwrap_or_else(|err| {
-            eprintln!("Problem parsing delay: {}", err);
-            print_usage(&program, &opts);
-            process::exit(1);
-        });
-
-    let devices = if !matches.free.is_empty() {
-        matches.free.clone()
-    } else {
-        print_usage(&program, &opts);
-        return;
-    };
-    println!("delay_sec = {}", delay_sec);
-    println!("devices = {:?}", devices);
 }
